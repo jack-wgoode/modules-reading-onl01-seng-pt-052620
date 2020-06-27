fe29c3ef4f785fdbd4a793f72f1d97ef5b162805
Jack Goode <jack.w.goode@icloud.com>
2020-06-27 19:08:31 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,8 +1,10 @@
-require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
 require_relative './class_methods_module.rb' 
 
 class Kid
-  include Dance
+  extend FancyDance::ClassMethods 
+  include FancyDance::InstanceMethods
+  #include Dance
   
   attr_accessor :name 
   
